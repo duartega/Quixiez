b1caecb45902e1b0d9fe7f23609f427b73a7ecc0
Joseph Missamore <joemissamore@live.com>
2019-05-22 17:47:58 -0700

    Chat Bubble Updated

--- src/our-components/ChatBubble.tsx
+++ src/our-components/ChatBubble.tsx
@@ -6,33 +6,82 @@ interface Props {
   badgeLabel: string;
   message: string;
   timePassed: string;
+  inverted: boolean;
 }
 
-class ChatBubble extends React.Component<Props> {
-  render() {
-    const { badgeColor, badgeLabel, message, timePassed } = this.props;
-    return (
-      <Row className="mx-xl-xl">
-        <Col>
-          <Card className="card-timeline  card-plain ">
-            <CardBody>
-              <div className="timeline-panel our-timeline-inverted">
-                <div className="timeline-heading">
-                  <Badge color={badgeColor}>{badgeLabel}</Badge>
-                </div>
-                <div className="timeline-body">
-                  <p>{message}</p>
-                </div>
-                <h6>
-                  <i className="ti-time" />
-                  {timePassed} ago via quixiez
-                </h6>
+export const ChatBubble = (props: Props) => {
+  const { badgeColor, badgeLabel, message, timePassed, inverted } = props;
+  return (
+    <Row className="mx-xl-xl">
+      <Col>
+        <Card className="card-timeline card-plain card-no-margin">
+          <CardBody>
+            <div
+              className={
+                inverted
+                  ? "timeline-panel our-timeline-inverted"
+                  : "timeline-panel our-timeline"
+              }
+            >
+              <div className="timeline-heading">
+                <Badge color={badgeColor} className="bubble-badge">
+                  {badgeLabel}
+                </Badge>
               </div>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    );
-  }
-}
-export default ChatBubble;
+              <div className="timeline-body">
+                <p>{message}</p>
+              </div>
+              <h6>
+                <i className="ti-time" />
+                {timePassed} ago
+              </h6>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+// class ChatBubble extends React.Component<Props> {
+//   render() {
+//     const {
+//       badgeColor,
+//       badgeLabel,
+//       message,
+//       timePassed,
+//       inverted
+//     } = this.props;
+//     return (
+//       <Row className="mx-xl-xl">
+//         <Col>
+//           <Card className="card-timeline card-plain card-no-margin">
+//             <CardBody>
+//               <div
+//                 className={
+//                   inverted
+//                     ? "timeline-panel our-timeline-inverted"
+//                     : "timeline-panel our-timeline"
+//                 }
+//               >
+//                 <div className="timeline-heading">
+//                   <Badge color={badgeColor} className="bubble-badge">
+//                     {badgeLabel}
+//                   </Badge>
+//                 </div>
+//                 <div className="timeline-body">
+//                   <p>{message}</p>
+//                 </div>
+//                 <h6>
+//                   <i className="ti-time" />
+//                   {timePassed} ago
+//                 </h6>
+//               </div>
+//             </CardBody>
+//           </Card>
+//         </Col>
+//       </Row>
+//     );
+//   }
+// }
+// export default ChatBubble;
--- src/views/pages/Conversations.jsx
+++ src/views/pages/Conversations.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // react plugin used to create switch buttons
 import Switch from "react-bootstrap-switch";
-import ChatBubble from "../../our-components/ChatBubble";
+import { ChatBubble } from "../../our-components/ChatBubble";
 
 // reactstrap components
 import {
@@ -29,51 +29,81 @@ import {
 } from "reactstrap";
 
 class Widgets extends React.Component {
+  constructor() {
+    super();
+    this.showNormalBubble = false;
+  }
+
+  getNormalBubble = () =>
+    this.showNormalBubble ? (
+      <Row className="mx-xl-xl">
+        <Col>
+          <Card className="card-timeline  card-plain ">
+            <CardBody>
+              <ul className="timeline timeline-simple">
+                {/* <ul className="our-timeline"> */}
+                <li
+                  className="timeline-inverted"
+                  // style={{ backgroundColor: "red" }}
+                >
+                  {/** this will invert the message bubble if class name taken out */}
+                  <div className="timeline-badge danger"> A </div>
+                  {/** For first letter of the persons name */}
+                  <div className="timeline-panel">
+                    <div className="timeline-heading">
+                      <Badge color="danger">Alexis</Badge>
+                    </div>
+                    <div className="timeline-body">
+                      <p>
+                        YES, I would like to also get a metal grinder. 1 ounce
+                        of blue diesel and 3 grams of grape pie as well.
+                      </p>
+                    </div>
+                    <h6>
+                      <i className="ti-time" />
+                      11 hours ago via quixiez
+                    </h6>
+                  </div>
+                </li>
+              </ul>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    ) : null;
   render() {
     return (
       <>
         <div className="content">
-          <Row className="mx-xl-xl">
-            <Col>
-              <Card className="card-timeline  card-plain ">
-                <CardBody>
-                  <ul className="timeline timeline-simple">
-                    {/* <ul className="our-timeline"> */}
-                    <li
-                      className="timeline-inverted"
-                      // style={{ backgroundColor: "red" }}
-                    >
-                      {/** this will invert the message bubble if class name taken out */}
-                      <div className="timeline-badge danger"> A </div>
-                      {/** For first letter of the persons name */}
-                      <div className="timeline-panel">
-                        <div className="timeline-heading">
-                          <Badge color="danger">Alexis</Badge>
-                        </div>
-                        <div className="timeline-body">
-                          <p>
-                            YES, I would like to also get a metal grinder. 1
-                            ounce of blue diesel and 3 grams of grape pie as
-                            well.
-                          </p>
-                        </div>
-                        <h6>
-                          <i className="ti-time" />
-                          11 hours ago via quixiez
-                        </h6>
-                      </div>
-                    </li>
-                  </ul>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <ChatBubble
-            badgeColor="warning"
-            badgeLabel="Gabe"
-            message="A message"
-            timePassed="10 hours"
-          />
+          {this.getNormalBubble()}
+          <div style={{ backgroundColor: "blue" }}>
+            <ChatBubble
+              badgeColor="warning"
+              badgeLabel="Gabe"
+              message="A message"
+              timePassed="10 hours"
+              inverted
+            />
+            <ChatBubble
+              badgeColor="info"
+              badgeLabel="Joe"
+              message="What's up Gabe?"
+              timePassed="7 Days"
+            />
+            <ChatBubble
+              badgeColor="info"
+              badgeLabel="Joe"
+              message="Hey Man Reply!"
+              timePassed="7 Days"
+            />
+            <ChatBubble
+              badgeColor="warning"
+              badgeLabel="Gabe"
+              message="Hey Joe"
+              timePassed="7 Days"
+              inverted
+            />
+          </div>
         </div>
       </>
     );
