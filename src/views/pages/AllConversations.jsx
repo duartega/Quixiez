cd07b9d7c3feca0458a532703ddc32be559f4be5
Joseph Missamore <joemissamore@live.com>
2019-05-24 14:57:13 -0700

    fixed allconversations page

--- src/views/pages/AllConversations.jsx
+++ src/views/pages/AllConversations.jsx
@@ -5,10 +5,28 @@ import { ChatFooter } from "../../our-components/ChatFooter";
 import Conversations from "./Conversations";
 import BusinessHours from "../tables/BusinessHoursTable";
 import ConversationsList from "../../our-components/Tables/ConversationsList";
+
 class AllConversations extends React.Component {
+  constructor(props) {
+    super(props);
+    /** Our custom height we are taking out */
+    this.customHeight = 50;
+    /**
+     * Taking into account the header total size
+     *
+     * header height = 46
+     * header padding top = 10
+     * header padding bottom = 10
+     *
+     * total height = 66
+     */
+
+    this.headerHeightWithPadding = 66;
+  }
   componentWillMount() {
     console.log(window.innerHeight);
   }
+
   render() {
     return (
       <Row style={{ overflow: "hidden" }} className="content">
@@ -16,7 +34,15 @@ class AllConversations extends React.Component {
           <ConversationsList />
         </Col>
 
-        <Col style={{ overflow: "auto", height: window.innerHeight - 50 }}>
+        <Col
+          style={{
+            overflow: "auto",
+            height:
+              window.innerHeight -
+              this.customHeight -
+              this.headerHeightWithPadding
+          }}
+        >
           <Conversations />
         </Col>
       </Row>
