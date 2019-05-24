4ab724aa2e28cbf360da4d582e26456b5b996242
Joseph Missamore <joemissamore@live.com>
2019-05-24 14:34:03 -0700

    Settings modularized

---
+++ src/our-components/Settings/Address.tsx
@@ -0,0 +1,129 @@
+import React, { ChangeEventHandler } from "react";
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+  Label
+} from "reactstrap";
+
+interface Props {
+  streetDefaultValue: string;
+  cityDefaultValue: string;
+  stateDefaultValue: string;
+  zipDefaultValue: string;
+  onChange: ChangeEventHandler;
+  onClick: React.MouseEventHandler<any>;
+  showStreet: boolean;
+}
+
+export const Address = (props: Props) => {
+  const {
+    streetDefaultValue,
+    cityDefaultValue,
+    stateDefaultValue,
+    zipDefaultValue,
+    onChange,
+    onClick,
+    showStreet
+  } = props;
+  return (
+    <Row className="mx-xl-xl">
+      <Col md="8">
+        <Card>
+          <CardHeader>
+            <h2 className="title">Company Address</h2>
+          </CardHeader>
+          <CardBody>
+            <Form>
+              <Row>
+                <Col className="px-md-3" md="12">
+                  <FormGroup>
+                    <label>Company Address</label>
+                    <Input
+                      defaultValue={streetDefaultValue}
+                      type="text"
+                      name="street"
+                      onChange={onChange}
+                      placeholder="No Address Set"
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="pr-md-1" md="4">
+                  <FormGroup>
+                    <label>City</label>
+                    <Input
+                      placeholder="No City Set"
+                      defaultValue={cityDefaultValue}
+                      type="text"
+                      name="city"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="px-md-1" md="4">
+                  <FormGroup>
+                    <label>State</label>
+                    <Input
+                      placeholder="No State Set"
+                      defaultValue={stateDefaultValue}
+                      type="text"
+                      name="state"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="pl-md-1" md="4">
+                  <FormGroup>
+                    <label>Postal Code</label>
+                    <Input
+                      placeholder="No Postal Code Set"
+                      defaultValue={zipDefaultValue}
+                      type="number"
+                      name="zip"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="pl-md-3" md="4">
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        defaultChecked={showStreet}
+                        type="checkbox"
+                        // value={this.state.showStreet}
+                        name="showStreet"
+                        onChange={onChange}
+                      />
+                      <span className="form-check-sign" />
+                      Show Street Address?
+                    </Label>
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </CardBody>
+          <CardFooter className="text-right">
+            <Button
+              className="btn-fill"
+              color="success"
+              type="submit"
+              onClick={onClick}
+            >
+              Save
+            </Button>
+          </CardFooter>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
---
+++ src/our-components/Settings/General.tsx
@@ -0,0 +1,131 @@
+import React, { ChangeEventHandler } from "react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
+
+interface Props {
+  companyNameDefaultValue: string;
+  quixiezNumberDefaultValue: string;
+  phoneNumberDefaultValue: string;
+  emailDefaultValue: string;
+  websiteUrlDefaultValue: string;
+
+  onChange: ChangeEventHandler;
+  onClick: React.MouseEventHandler<any>;
+}
+
+export const General = (props: Props) => {
+  const {
+    companyNameDefaultValue,
+    quixiezNumberDefaultValue,
+    phoneNumberDefaultValue,
+    emailDefaultValue,
+    websiteUrlDefaultValue,
+    onChange,
+    onClick
+  } = props;
+
+  return (
+    <Row className="mx-xl-xl">
+      <Col md="8">
+        <Card>
+          <CardHeader>
+            <h2 className="title">Company Overview</h2>
+          </CardHeader>
+          <CardBody>
+            <Form>
+              <Row>
+                <Col className="pr-md-1" md="5">
+                  <FormGroup>
+                    <label>Company</label>
+                    <Input
+                      defaultValue={companyNameDefaultValue}
+                      type="text"
+                      name="companyName"
+                      onChange={onChange}
+                      placeholder="No Company Name Set"
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="px-md-1" md="3">
+                  <FormGroup>
+                    <label>Quixiez Number</label>
+                    {/* should change to number */}
+
+                    <Input
+                      placeholder="No Phone Number Set"
+                      defaultValue={quixiezNumberDefaultValue}
+                      type="text"
+                      name="quixiezNumber"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="pl-md-1" md="4">
+                  <FormGroup>
+                    <label>Bussiness Number</label>
+
+                    <Input
+                      placeholder="No Phone Number Set"
+                      defaultValue={phoneNumberDefaultValue}
+                      type="text"
+                      name="phoneNumber"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col className="pr-md-1" md="6">
+                  <FormGroup>
+                    <label>Business Email</label>
+
+                    <Input
+                      placeholder="No Email Set"
+                      defaultValue={emailDefaultValue}
+                      type="email"
+                      name="email"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col className="pl-md-1" md="6">
+                  <FormGroup>
+                    <label>Business Website</label>
+
+                    <Input
+                      placeholder="No Website Set"
+                      defaultValue={websiteUrlDefaultValue}
+                      type="url"
+                      name="websiteUrl"
+                      onChange={onChange}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </CardBody>
+          <CardFooter className="text-right">
+            <Button
+              className="btn-fill"
+              color="success"
+              type="submit"
+              onClick={onClick}
+            >
+              Save
+            </Button>
+          </CardFooter>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
