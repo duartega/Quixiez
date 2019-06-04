1c9814035c767d7b7ce85fff01b16febe56e9cbc
Joseph Missamore <joemissamore@live.com>
2019-06-04 13:12:48 -0700

    Rendering active cell

--- src/our-components/Components/Conversations List/ConversationCell.jsx
+++ src/our-components/Components/Conversations List/ConversationCell.jsx
@@ -1,10 +1,11 @@
 // This is a dumb component for the conversation cell
 // on the conversation list
-import React from 'react';
-import { Card, Row, Col, CardBody } from 'reactstrap';
+import React from "react";
+import { Card, Row, Col, CardBody } from "reactstrap";
+
+import { connect } from "react-redux";
 
 class ConversationCell extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -14,58 +15,90 @@ class ConversationCell extends React.Component {
     };
   }
 
+  componentDidMount() {
+    console.log("CONVERSATIONCELL DID MOUNT", this.props.idx);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.active === false &&
+      this.props.idxOfConversationToRender === this.props.idx
+    ) {
+      this.setState({ active: true });
+    } else if (
+      this.state.active &&
+      this.props.idxOfConversationToRender !== this.props.idx
+    ) {
+      this.setState({ active: false });
+    }
+  }
+
   render() {
-    const {props} = this.props;
-    let readIndicator = props.unread;    
+    const { props } = this.props;
+    let readIndicator = props.unread;
 
-    return ( <> 
-      <Card className="card-cell" style={{
-        background: this.state.active ? "orange": "", 
-        marginBottom: "0px",
-        boxShadow: "0 0px 0px 0px rgba(0, 0, 0, 0)"
-      }}> 
-      <Row style={{marginLeft: "0px"}} onClick={() => { this.props.renderConvo(props.id); this.setState({active: true})}}>
+    return (
+      <>
+        <Card
+          className="card-cell"
+          style={{
+            background: this.state.active ? "orange" : "",
+            marginBottom: "0px",
+            boxShadow: "0 0px 0px 0px rgba(0, 0, 0, 0)"
+          }}
+        >
+          <Row
+            style={{ marginLeft: "0px" }}
+            onClick={() => {
+              this.props.renderConvo(props.id);
+              // this.setState({ active: true });
+            }}
+          >
+            <Row className="messageListContainer">
+              <div className="nameCircle">{props.firstInitial}</div>
+            </Row>
 
-          <Row className="messageListContainer">
-              <div className="nameCircle">
-                {props.firstInitial}
-              </div>
-          </Row>
-
-          {/** right side of messagelist */}
-          <Col >
-            <Row style={{ paddingTop: "30px", marginRight: "15px"}}>
-                    <Col md="7" >
-                    {readIndicator ? <span className="newMessageDot"></span> : null}
-                      <p style={{display: "inline-block"}}>
-                        {props.name}
-                      </p>
-                    </Col>
-                    <Col md="5" style={{paddingRight: "0px", textAlign: "right"}}>
-                      <i className="ti-time"/> {props.time}
-                    </Col>
-                  </Row>
-                <Row>
-                  <CardBody style={{
+            {/** right side of messagelist */}
+            <Col>
+              <Row style={{ paddingTop: "30px", marginRight: "15px" }}>
+                <Col md="7">
+                  {readIndicator ? <span className="newMessageDot" /> : null}
+                  <p style={{ display: "inline-block" }}>{props.name}</p>
+                </Col>
+                <Col md="5" style={{ paddingRight: "0px", textAlign: "right" }}>
+                  <i className="ti-time" /> {props.time}
+                </Col>
+              </Row>
+              <Row>
+                <CardBody
+                  style={{
                     paddingTop: "0px",
                     paddingLeft: "15px"
-                  }}>
-                    <p>
-                      {props.message}
-                    </p>
-                  </CardBody>
-                </Row>
-        </Col>
-        </Row>
-      </Card>
-      {/** row seperator */}
-      <Card style={{backgroundColor: '#303245', marginBottom: "0px",
-        boxShadow: "0 0px 0px 0px rgba(0, 0, 0, 0)"}} >
-        <CardBody style={{padding: "2px"}}></CardBody> 
-      </Card>
-     </>
-        );
-    }
+                  }}
+                >
+                  <p>{props.message}</p>
+                </CardBody>
+              </Row>
+            </Col>
+          </Row>
+        </Card>
+        {/** row seperator */}
+        <Card
+          style={{
+            backgroundColor: "#303245",
+            marginBottom: "0px",
+            boxShadow: "0 0px 0px 0px rgba(0, 0, 0, 0)"
+          }}
+        >
+          <CardBody style={{ padding: "2px" }} />
+        </Card>
+      </>
+    );
+  }
 }
 
-export default ConversationCell;
+const mapStateToProps = ({ conversation: { idxOfConversationToRender } }) => ({
+  idxOfConversationToRender
+});
+
+export default connect(mapStateToProps)(ConversationCell);
--- src/our-components/Components/Conversations List/ConversationList.jsx
+++ src/our-components/Components/Conversations List/ConversationList.jsx
@@ -1,100 +1,103 @@
-import React from 'react';
+import React from "react";
 import ConversationCell from "./ConversationCell";
-import testData from './testData';
+import testData from "./testData";
 import { connect } from "react-redux";
 import { setConversationToRender } from "../../../redux/actions/conversations";
 import * as StatusInfo from "../../Tables/StatusInfo";
-import { PopOverLeft } from '../PopOverLeft';
+import { PopOverLeft } from "../PopOverLeft";
 import { Button } from "reactstrap";
 
 class ConversationList extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            name: "",
-            time: "",
-            message: "",
-            firstInitial: "",
-            tableData: []
-        }
+    this.state = {
+      name: "",
+      time: "",
+      message: "",
+      firstInitial: "",
+      tableData: []
     };
+  }
 
-    componentDidMount() {
-        const { allConversations } = this.props;
-        if (allConversations) {
-          this.mapConversationsToTable();
-        }
-      }
+  componentDidMount() {
+    const { allConversations } = this.props;
+    if (allConversations) {
+      this.mapConversationsToTable();
+    }
+  }
 
-      mapConversationsToTable = () => {
-        let conversationsArray = [];
-    
-        const { allConversations } = this.props;
-    
-        // Create an array for our conversations
-        conversationsArray = allConversations.map((aConversation, idx) => {
-          let { consumerUser, messages } = aConversation;
-          let fname = consumerUser.firstName + " " + consumerUser.lastName;
-          let lastMessage = messages[messages.length - 1].content;
-          let timeRecieved = messages[messages.length - 1].created;
-          let firstInitial = fname.charAt(0);
-    
-          let time = StatusInfo.calculateTime(timeRecieved);
-    
-          return {
-            id: idx,
-            name: fname,
-            message: lastMessage,
-            time: time,
-            firstInitial: firstInitial
-          };
-        });
-        console.log("THIS IS THE ARRAY: ", conversationsArray)
-        // Set the state so we can render our conversations
-        this.setState({ tableData: conversationsArray });
+  mapConversationsToTable = () => {
+    let conversationsArray = [];
+
+    const { allConversations } = this.props;
+
+    // Create an array for our conversations
+    conversationsArray = allConversations.map((aConversation, idx) => {
+      let { consumerUser, messages } = aConversation;
+      let fname = consumerUser.firstName + " " + consumerUser.lastName;
+      let lastMessage = messages[messages.length - 1].content;
+      let timeRecieved = messages[messages.length - 1].created;
+      let firstInitial = fname.charAt(0);
+
+      let time = StatusInfo.calculateTime(timeRecieved);
+
+      return {
+        id: idx,
+        name: fname,
+        message: lastMessage,
+        time: time,
+        firstInitial: firstInitial
       };
-      
-      handleViewConversation = idx => {
-          console.log("IDX: ", idx)
-        const { setConversationToRender } = this.props;
-        setConversationToRender(idx);
-        // const { history } = this.props;
-        // console.log("HISTORY: ", history)
-        // history.push("/admin/conversations/messages");
-        
-      };
-      
-    render() {
-      // Fix dot using testData
-        return (
-            <>
-            <div className="content">
-                {this.state.tableData.map((data, idx)=>
-                    <ConversationCell key={idx} props={data} renderConvo={this.handleViewConversation} />
-                )}
-                {/* {testData.map((data, idx)=>
+    });
+    console.log("THIS IS THE ARRAY: ", conversationsArray);
+    // Set the state so we can render our conversations
+    this.setState({ tableData: conversationsArray });
+  };
+
+  handleViewConversation = idx => {
+    console.log("IDX: ", idx);
+    const { setConversationToRender } = this.props;
+    setConversationToRender(idx);
+    // const { history } = this.props;
+    // console.log("HISTORY: ", history)
+    // history.push("/admin/conversations/messages");
+  };
+
+  render() {
+    // Fix dot using testData
+    console.log("CONVERSATION LIST MOUNTED");
+    return (
+      <>
+        <div className="content">
+          {this.state.tableData.map((data, idx) => (
+            <ConversationCell
+              key={idx}
+              idx={idx}
+              props={data}
+              renderConvo={this.handleViewConversation}
+            />
+          ))}
+          {/* {testData.map((data, idx)=>
                     <ConversationCell key={idx} props={data} renderConvo={this.handleViewConversation} />
                 )} */}
-            </div>
-                
-            </>
-        );
-    };
+        </div>
+      </>
+    );
+  }
 }
 
 // export default ConversationList;
 
 const mapStateToProps = ({ conversation: { allConversations } }) => ({
-    allConversations
-  });
-  
-  const mapDispatchToProps = dispatch => ({
-    setConversationToRender: idx => dispatch(setConversationToRender(idx))
-  });
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(ConversationList);
+  allConversations
+});
+
+const mapDispatchToProps = dispatch => ({
+  setConversationToRender: idx => dispatch(setConversationToRender(idx))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConversationList);
